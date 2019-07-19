567dbade47e3558e38847a7ae076a805e4768ca8
Fuadtag <Fuadjt@code.edu.az>
2019-07-19 14:23:37 +0400

    HTML done

---
+++ js/main.js
@@ -0,0 +1,14 @@
+"use strict";
+let student = [];
+let inputs = document.getElementsByTagName(input)
+
+
+
+
+function Students(name, surname, email, phone, age) {
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+    this.age = age;
+}
